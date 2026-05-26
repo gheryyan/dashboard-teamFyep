df2b130863809ba791add1a1fe49f7baea4fbc54
404 <washesyagws@gmail.com>
2026-05-26 12:30:22 +0700

    perbaruin

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "https://webgws-two.my.id/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
   withCredentials: true, // wajib untuk Sanctum
   headers: {
     "Content-Type": "application/json",
