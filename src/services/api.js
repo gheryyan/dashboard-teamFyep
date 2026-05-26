b61236b884e8ff3e700a27d74703a0ba30789ced
404 <washesyagws@gmail.com>
2026-05-26 12:28:02 +0700

    api nya

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://webgws-two.my.id/api",
   withCredentials: true, // wajib untuk Sanctum
   headers: {
     "Content-Type": "application/json",
